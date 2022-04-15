53a43922a94a021901a7e83dddd2d14ed4efda80
gdcristea <georgedanielcristea@gmail.com>
2022-04-15 12:25:39 +0300

    Commit

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ const error = document.querySelector(".email-not-valid");
 input.addEventListener("input", checkEmail);
 
 function checkEmail() {
-    if (!input.value.match(validRegExp)) {
+    if (input.value == "") {
         error.style.display = "block";
         input.classList.add("email-border-error");
-    } else {
+        error.innerHTML = "Oops! Please add your email";
+    }
+    else if ((input.value !== "") && (!input.value.match(validRegExp))) {
+        error.style.display = "block";
+        input.classList.add("email-border-error");
+        error.innerHTML = "Oops! That doesn\’t look like an email address";
+    } 
+    else {
         error.style.display = "none";
         input.classList.remove("email-border-error");
     }
 }
-
-
-
